652bbd66f984414bfc19053e3c6ecfbf5f151414
SuleymanKahraman <suleymankahraman1993@gmail.com>
2022-12-10 21:04:38 +0300

    added first commit

--- KutuphaneOtomasyon/Models/DataHelper.cs
+++ KutuphaneOtomasyon/Models/DataHelper.cs
@@ -14,6 +14,7 @@ namespace KutuphaneOtomasyon
     {
         public static string conStr;
         public readonly SqlConnection connect;
+        //deneme mesajı
         public DataHelper()
         {
             conStr = "Data Source=LAPTOP-VMEL2LCP\\SQLEXPRESS01;Initial Catalog=kutuphane;Integrated Security=True";
